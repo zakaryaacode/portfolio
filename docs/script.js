9181cdd09107a53a08dce7d3be04cc8aca74c16e
zakarya <zakarya.code@gmail.com>
2025-05-17 20:40:17 +0200

    version 1

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,2 @@
-document.getElementById("contact-form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  alert("Thank you for your message! I will get back to you soon.");
-});
+
+ 
